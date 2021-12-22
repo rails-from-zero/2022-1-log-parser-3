7c1b64b728800ee5797e30dd7678f54717010980
Daniel Mircea <daniel@viseztrance.com>
2021-12-22 22:56:48 +0200

    Start of episode

---
+++ lib/log_parser.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "log_parser/version"
+require_relative "log_parser/file_reader"
+require_relative "log_parser/entry"
+require_relative "log_parser/tokenizer"
+require_relative "log_parser/accumulator"
+require_relative "log_parser/aggregator"
+require_relative "log_parser/aggregator_result"
+
+module LogParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
